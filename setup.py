a40edbd011521298c7ad8735f596fed5e37cf959
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2022-12-11 19:29:40 +0000

    fix(docs): deploy

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mkdocs-meta-manager',
     version='0.1.0',
-    description='MkDocs plugin for managing meta tags across folders and files.',
+    description='MkDocs plugin for managing meta tags across folders and files.',    
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords='mkdocs meta manager',
-    url='https://github.com/timmeinerzhagen/mkdocs-meta-manager/',
+    url='https://github.com/timmeinerzhagen/mkdocs-meta-manager',
     author='Tim Jonas Meinerzhagen',
     author_email='tim@meinerzhagen.me',
     license='MIT',
     license_files = ('LICENSE'),
+    classifiers=[
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: MIT License",
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
+    ],
     python_requires='>=3.4',
     install_requires=[
-        'mkdocs>=0.17',
+        'mkdocs>=1.0',
         'jinja2'
     ],
     packages=find_packages(),
