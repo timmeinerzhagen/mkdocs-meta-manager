b15d37dee38f827d36498f94a3ede2e105dc416b
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2022-12-11 17:23:55 +0000

    feat: propagate meta

--- mkdocs_meta_manager_plugin/plugin.py
+++ mkdocs_meta_manager_plugin/plugin.py
@@ -1,22 +1,44 @@
-import re
-from os import environ
-from datetime import datetime
+import yaml
 
 from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
 
 class MetaManagerPlugin(BasePlugin):
     config_scheme = (
-        ('meta_filename', config_options.Type(str)),
+        ('meta_filename', config_options.Type(str, default='.meta.yml')),
     )
+
+    meta_files = {}
 
     def __init__(self):
         self.enabled = True
+
+    def on_files(self, files, config):
+        for file in files:
+            if file.src_path.endswith(self.config['meta_filename']):
+                print(file)
+                with open(file.src_path, "r") as stream:
+                    try:
+                        self.meta_files[file.src_path] = yaml.safe_load(stream)
+                    except yaml.YAMLError as exc:
+                        print(exc)
+                
+                files.remove(file)
+        return files
+        
 
     def on_page_markdown(self, markdown, page, config, files):
         if not self.enabled:
             return markdown
 
         print(page)
+
+        for part in page.file.src_path.split('/'):
+            if part in self.meta_files:
+                print(part)
+                for key, value in self.meta_files[part].items():
+                    print(key, value)
+                    if not page.meta[key]:
+                        page.meta[key] = value
 
         return markdown
