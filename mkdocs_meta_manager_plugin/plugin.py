3e1a356707df215ad41dd096189daa51c2c539a3
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2022-12-11 16:55:07 +0000

    feat: initial commit

---
+++ mkdocs_meta_manager_plugin/plugin.py
@@ -0,0 +1,23 @@
+import re
+from os import environ
+from datetime import datetime
+
+from mkdocs.config import config_options
+from mkdocs.plugins import BasePlugin
+
+
+class GitRevisionDatePlugin(BasePlugin):
+    config_scheme = (
+        ('meta_filename', config_options.Type(str)),
+    )
+
+    def __init__(self):
+        self.enabled = True
+
+    def on_page_markdown(self, markdown, page, config, files):
+        if not self.enabled:
+            return markdown
+
+        print(markdown)
+
+        return markdown
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkdocs-meta-manager',
+    version='0.0.1',
+    description='MkDocs plugin for managing meta tags across folders and files.',
+    keywords='mkdocs meta manager',
+    url='https://github.com/timmeinerzhagen/mkdocs-meta-manager/',
+    author='Tim Jonas Meinerzhagen',
+    author_email='tim@meinerzhagen.me',
+    license='MIT',
+    license_files = ('LICENSE'),
+    python_requires='>=3.4',
+    install_requires=[
+        'mkdocs>=0.17',
+        'jinja2'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'meta-manager = mkdocs_meta_manager_plugin.plugin:MetaManagerPlugin'
+        ]
+    }
+)
